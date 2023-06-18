733abbe74e71fbc86daacd50596e1b29b71ca76c
Hokku <darksouls2make@gmail.com>
2023-06-19 00:10:13 +0400

    added dbContext

---
+++ IVQ.WebApi/Data/IVQDbContext.cs
@@ -0,0 +1,19 @@
+﻿using IVQ.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IVQ.WebApi.Data;
+
+public class IVQDbContext : DbContext
+{
+    public IVQDbContext(DbContextOptions<IVQDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseSerialColumns();
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Question> Questions { get; set; }
+}
