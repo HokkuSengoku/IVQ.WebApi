0f292f8d2dd38bf689f166c00d76544569f4a47d
Hokku <darksouls2make@gmail.com>
2023-06-22 17:32:35 +0400

    Added exceptions

---
+++ IVQ.WebApi/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace IVQ.WebApi.Exceptions;
+
+public class NotFoundException : StandartException
+{
+    public NotFoundException(string errorType, string message) 
+        : base(
+            "Specified resource was not found.",
+            errorType, 
+            message
+        )
+    {
+    }
+}
---
+++ IVQ.WebApi/Exceptions/StandartException.cs
@@ -0,0 +1,12 @@
+﻿namespace IVQ.WebApi.Exceptions;
+
+public class StandartException : Exception
+{
+    public StandartException(string title, string errorType, string message) : base(message)
+    {
+        Title = title;
+        ErrorType = errorType;
+    }
+    public string Title { get; }
+    public string ErrorType { get; }
+}
