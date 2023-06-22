eefe83ec9f1f93ee994cf774c5a0cddda097ab35
Hokku <darksouls2make@gmail.com>
2023-06-22 20:14:18 +0400

    Added httpStatucCode resolver

---
+++ IVQ.WebApi/Services/HttpStatusCodeResolver.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using IVQ.WebApi.Exceptions;
+
+namespace IVQ.WebApi.Services;
+
+public class HttpStatusCodeResolver
+{
+    public static HttpStatusCode Resolve(StandartException exception)
+    {
+        return exception switch
+        {
+            AlreadyExistsException => HttpStatusCode.Conflict,
+            NotFoundException => HttpStatusCode.NotFound,
+            _ => HttpStatusCode.NotImplemented,
+        };
+    }
+}
